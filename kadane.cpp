89fa2e5fcf28de2a25eac44cdcb08fb9f34896bc
Richik1 <55250423+Richik1@users.noreply.github.com>
2020-11-08 13:30:44 +0530

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
       int arr[10]={1,2,3,3,6,7,8,9,3,5};
-      int res=INT_MIN,curr_sum=arr[0];
+      int res=arr[0],curr_sum=arr[0];
       for(int i=1;i<10;i++){
-         curr_sum=max(curr_sum,curr_sum+arr[i]);
+         curr_sum=max(arr[i],curr_sum+arr[i]);
          res=max(res,curr_sum);
       }
       cout<<res<<" ";
